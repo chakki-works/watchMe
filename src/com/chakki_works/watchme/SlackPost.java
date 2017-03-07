f6171526ef6e503a5f5aea5ca77412fa1ee18b82
Hironsan <light.tree.1.13@gmail.com>
2017-03-07 09:29:31 +0900

    add error message

--- src/com/chakki_works/watchme/SlackPost.java
+++ src/com/chakki_works/watchme/SlackPost.java
@@ -1,5 +1,7 @@
 package com.chakki_works.watchme;
-
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -32,8 +34,34 @@ public class SlackPost {
 
             if (conn.getResponseCode() == 200 && readInputStreamToString(conn).equals("ok")) {
                 //Messages.showMessageDialog(project, "Message Sent.", "Information", SlackStorage.getSlackIcon());
+                Notifications.Bus.notify(
+                        new Notification("not error", "Message sent!", "Error message was sent!", NotificationType.INFORMATION)
+                );
             }
             else {
+                int status_code = conn.getResponseCode();
+                String title = "";
+                String content = "";
+                if (status_code == 400) {
+                    title = "invalid_payload";
+                    content = "the data sent in your request cannot be understood as presented; verify your content body matches your content type and is structurally valid.";
+                } else if (status_code == 403) {
+                    title = "action_prohibited";
+                    content = "the team associated with your request has some kind of restriction on the webhook posting in this context.";
+                } else if (status_code == 404) {
+                    title = "channel_not_found";
+                    content = "the channel associated with your request does not exist.";
+                } else if (status_code == 410) {
+                    title = "channel_is_archived";
+                    content = "the channel has been archived and doesn't accept further messages, even from your incoming webhook.";
+                } else if (status_code == 500) {
+                    title = "rollup_error";
+                    content = "something strange and unusual happened that was likely not your fault at all.";
+                }
+
+                Notifications.Bus.notify(
+                        new Notification("error", title, content, NotificationType.ERROR)
+                );
                 //Messages.showMessageDialog(project, "Message not sent, check your configuration.", "Error", Messages.getErrorIcon());
             }
 
